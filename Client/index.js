204f27415465bc898dbadc440eba19281bd29384
Sávio Luiz <savioluiz546@gmail.com>
2025-07-28 01:20:57 -0300

    Colocando a tela Home

--- Client/index.js
+++ Client/index.js
@@ -3,3 +3,24 @@ function login() {
     console.log("Busca")
     
 }
+function telaConsulta() {
+    window.location.href= "../Home/consultar.html"
+    console.log("Consultar")
+
+}
+function telaCadastrarVeic() {
+    window.location.href= "../Home/veiculo.html"
+    console.log("Consultar")
+
+}
+function telaCadastrarDesp() {
+    window.location.href= "../Home/despesas.html"
+    console.log("Consultar")
+
+}
+function telaArquivar() {
+    window.location.href= "../Home/arquivar.html"
+    console.log("Consultar")
+
+}
+
